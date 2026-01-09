4ac8cb898b5249e058075c385c11e51d500a4de3
Kiaz0r <wsrocha.wr@outlook.com>
2026-01-08 23:23:11 -0300

    Incluí as constantes do núcleo da calculadora.

---
+++ core/constants.py
@@ -0,0 +1,7 @@
+MAX_INTEGER_DIGITS = 30
+MAX_TOTAL_SIGNIFICANT = 60
+RESULT_MAX_FRACTION_DIGITS = 12
+APP_VERSION = '1.0.0-rc.1'
+APP_AUTHOR = 'Kiaz0r'
+APP_WEBSITE = 'https://github.com/Kiaz0r/python-calculator'
+APP_SITE = 'https://kiaz0r.com.br/'
