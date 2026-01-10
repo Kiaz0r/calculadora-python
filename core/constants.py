45bc678127c1263fd6e59011382d4845dc65b8e9
Kiaz0r <wsrocha.wr@outlook.com>
2026-01-10 00:52:46 -0300

    Removi a constante APP_WEBSITE não utilizada no módulo de constantes.

--- core/constants.py
+++ core/constants.py
@@ -3,5 +3,4 @@ MAX_TOTAL_SIGNIFICANT = 60
 RESULT_MAX_FRACTION_DIGITS = 12
 APP_VERSION = '1.0.0-rc.1'
 APP_AUTHOR = 'Kiaz0r'
-APP_WEBSITE = 'https://github.com/Kiaz0r/python-calculator'
 APP_SITE = 'https://kiaz0r.com.br/'
